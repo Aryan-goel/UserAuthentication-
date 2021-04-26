c4be77cfa074e94b785fbebf4f2bd7fc3f9a4d13
91639 <goel.aryan2406@gmail.com>
2021-04-26 22:15:29 +0530

    complete

---
+++ src/main/java/com/example/api/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    
+    User findByEmailAndPassword(String email,String password);
+}
